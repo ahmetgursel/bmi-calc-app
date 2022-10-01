a80f4a5a8f7a24c3c2b986e2319b2ff4b70bde0a
ahmetgursel <ahmetgursel@protonmail.com>
2022-10-01 22:13:15 +0300

    ✨ add bmi modal

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -15,15 +15,10 @@ function Calculator() {
     console.log('height', height);
   };
 
-  // TODO: input schema validation
-  // TODO: calculate bmi
-  // TODO: after calc bmi show modal & gauge
-  // FIXME: color schema
-
   return (
     <div className='container mt-10 mx-auto  px-2 py-2.5 space-y-6 md:mt-36'>
       <div className='flex items-center justify-center'>
-        <label htmlFor='name' className='block text-sm font-medium text-gray-700 mr-3 '>
+        <label htmlFor='name' className='block mr-3 text-sm font-medium text-gray-700 '>
           <img src={idendityIcon} width={'48px'} height={'48px'} alt='name' />
         </label>
         <div className='relative mt-1 rounded-lg shadow-sm'>
@@ -31,14 +26,14 @@ function Calculator() {
             type='text'
             name='name'
             id='name'
-            className='block w-full rounded-lg border-gray-300 pl-7 pr-12 focus:border-blue-100 focus:ring-blue-100 sm:text-xl'
+            className='block w-full pr-12 border-gray-300 rounded-lg pl-7 focus:border-blue-100 focus:ring-blue-100 sm:text-xl'
             onChange={(e) => setName(e.target.value)}
             placeholder='İsminizi giriniz..'
           />
         </div>
       </div>
       <div className='flex items-center justify-center'>
-        <label htmlFor='weight' className='block text-sm font-medium text-gray-700 mr-3'>
+        <label htmlFor='weight' className='block mr-3 text-sm font-medium text-gray-700'>
           <img src={weightIcon} width={'48px'} height={'48px'} alt='weight' />
         </label>
         <div className='relative mt-1 rounded-lg shadow-sm'>
@@ -46,14 +41,14 @@ function Calculator() {
             type='text'
             name='weight'
             id='weight'
-            className='block w-full rounded-lg border-gray-300 pl-7 pr-12 focus:border-blue-100 focus:ring-blue-100 sm:text-xl'
+            className='block w-full pr-12 border-gray-300 rounded-lg pl-7 focus:border-blue-100 focus:ring-blue-100 sm:text-xl'
             onChange={(e) => setWeight(e.target.value)}
             placeholder='Kilonuzu giriniz..'
           />
         </div>
       </div>
       <div className='flex items-center justify-center'>
-        <label htmlFor='height' className='block text-sm font-medium text-gray-700  mr-3'>
+        <label htmlFor='height' className='block mr-3 text-sm font-medium text-gray-700'>
           <img src={heightIcon} width={'48px'} height={'48px'} alt='height' />
         </label>
         <div className='relative mt-1 rounded-lg shadow-sm'>
@@ -61,7 +56,7 @@ function Calculator() {
             type='text'
             name='height'
             id='height'
-            className='block w-full rounded-lg border-gray-300 pl-7 pr-12 focus:border-blue-100 focus:ring-blue-100 sm:text-xl'
+            className='block w-full pr-12 border-gray-300 rounded-lg pl-7 focus:border-blue-100 focus:ring-blue-100 sm:text-xl'
             onChange={(e) => setHeight(e.target.value)}
             placeholder='Boyunuzu giriniz..'
           />
